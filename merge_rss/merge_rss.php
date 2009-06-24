574de6ca7828d9129d91f5c14d946d5c72a05c02
dmikam <dmikam@gmail.com>
2009-06-24 02:09:22 +0200

    Se puede gestionar las listas de RSS Para obtener al ARRAY de resultados de RSS sirve función mss_get_rss(<category_slug>,<type>)

--- merge_rss/merge_rss.php
+++ merge_rss/merge_rss.php
@@ -30,8 +30,6 @@ if ( is_admin() ){ // admin actions
 function mss_install() {
    global $wpdb;
    $table_name = $wpdb->prefix . "merge_rss";
-	
-
 	if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
 		$sql = "CREATE TABLE " . $table_name . " (
 			  id mediumint(9) NOT NULL AUTO_INCREMENT,
@@ -58,12 +56,96 @@ function mss_register_settings(){
 
 
 
+/*******************************************************************************
+										FUNCTIONS OF TEMPLATE
+*******************************************************************************/
 
+function hierarhical_select($list,$parent=0,$level=0){
+	$return = '';
+	foreach ($list as $item){
+		if ($item->parent==$parent){
+?>
+			<option value="<?php echo $item->slug;?>" ><?php echo str_repeat('-',$level)." ".$item->name;?></option>
+<?php
+			echo hierarhical_select($list,$item->term_id,$level+1);
+		}
+	}
+	return $return;
+}
 
+function mss_sort_rss_helper($a,$b) {
+	return strtotime($a['pubdate']) < strtotime($b['pubdate']);
+}
+function mss_get_rss($cat,$type){
+	global
+		$wpdb,
+		$mss_opt;	
+	include_once(ABSPATH . WPINC . '/rss.php');
+   $table_name = $wpdb->prefix . "merge_rss";
+	$where_array = array();
+	if (is_array($cat)){
+		$where_array[] = 'cat IN ("'. implode('","',$cat).'")';
+	}else{
+		$where_array[] = 'cat = "'.addslashes($cat).'"';
+	}
+	if (is_array($type)){
+		$where_array[] = 'type IN ("'. implode('","',$type).'")';
+	}else{
+		$where_array[] = 'type = "'.addslashes($type).'"';
+	}
+	$where = 'WHERE ('.implode(') AND (',$where_array).')';
+	$sql = "
+		SELECT
+			id,url,type,cat
+		FROM
+			$table_name
+		$where
+		ORDER BY 
+			id
+	";
+	$rss = $wpdb->get_results($sql);
+//	dump($sql);
+//	dump($rss);
+	$rss_list = array();
+	foreach($rss as $item){
+		$rss_list[] = fetch_rss($item->url);
+	}
+/*$rss_list[]= fetch_rss('http://www.versvs.net/node/feed');
+	$rss_list[]= fetch_rss('http://www.error500.net/node/feed');
+*/	
+	
 
+	$all_rss = array();
+	foreach($rss_list as $rss){
+		$all_rss = array_merge($all_rss,$rss->items);
+	}
+	usort($all_rss, mss_sort_rss_helper);
+	$return = array();
+	$cnt = 0;
+	foreach ($all_rss as $rss) {
+		if ($cnt>=$mss_opt['count']){
+			break;
+		}
+		$return[] = $rss;
+		//echo "$rss[title] -  $rss[pubdate] <br/>";
+		$cnt++;
+	}
+	return $return;
+}
 
+/*******************************************************************************
+										ADDITIONAL FUNCTIONS	
+*******************************************************************************/
 
+if (!function_exists('dump')){
+	$dump = create_function('$var','echo "<pre>";var_dump($var);echo "</pre>";');
+}
 
+function mss_block($val){
+	global $wpdb;
+//	return mysqli_real_escape_string($val);
+	return $wpdb->escape($val);
+}
 
 function mss_log($text) {
 	global $mss_opt;
--- merge_rss/mss_options.php
+++ merge_rss/mss_options.php
@@ -1,3 +1,58 @@
+<?
+	global $wpdb;
+	$wpdb->show_errors();
+   $table_name = $wpdb->prefix . "merge_rss";
+
+	if ($_POST['new_rss']){
+		$new_cat		= mss_block($_POST['mss']['category']);
+		$new_type	= mss_block($_POST['mss']['type']);
+		$new_url		= mss_block($_POST['mss']['url']);
+		
+/*		$sql = "
+			INSERT INTO 
+				$table_name
+				(url,type,cat)
+			VALUES
+				('$new_url','$new_type','$new_cat')
+		";
+		$wpdb->query($sql);
+*/
+		$wpdb->insert( $table_name, array( 'url' => $new_url, 'type' => $new_type, 'cat' => $new_cat ), array( '%s', '%s', '%s') );
+		$wpdb->print_error();
+	}
+	
+	if ($_POST['delete']){
+		foreach($_POST['delete'] as $id=>$value){
+			$wpdb->query( $wpdb->prepare( "
+				DELETE FROM 
+					$table_name
+				WHERE
+					id= %d", 
+				$id) 
+			);
+		}
+	}
+	
+	$categories =	get_categories(array(
+							'hide_empty'=>false
+							,'hierarchical'=>1
+						));
+	$cats = array();
+	foreach ($categories as $cat){
+		$cats[$cat->term_id] = $cat;
+	}
+	unset($categories);
+	
+	$sql = "
+		SELECT
+			id,url,type,cat
+		FROM
+			$table_name
+		ORDER BY 
+			id
+	";
+	$list = $wpdb->get_results($sql);
+?>
 <div class="wrap">
 	<h2>Merge RSS options page</h2>
 	<form method="post" action="options.php">
@@ -27,39 +82,42 @@
 	<form method="post" action="">
 		<table class="form-table">
 			<tr valign="top">
-				<th scope="row">&nbsp;</th>
 				<th scope="row">Categoría</th>
 				<th scope="row">Tipo</th>
 				<th scope="row">URL</th>
 			</tr>
 			<tr valign="top">
-				<td><input type="radio"	name="mss[new]" 				value="1"></td>
-				<td><input type="text"	name="mss[category][1]"		value=""	size="15" /></td>
-				<td><input type="text"	name="mss[type][1]"			value=""	size="5"/></td>
-				<td><input type="text"	name="mss[url][1]"			value=""	size="50" /></td>
-			</tr>
-			<tr valign="top">
-				<th scope="row" colspan="4">o</th>
-			</tr>
-			<tr valign="top">
-				<th scope="row">&nbsp;</th>
-				<th scope="row">Categoría</th>
-				<th scope="row">Tipo</th>
-				<th scope="row">URL</th>
-			</tr>
-			<tr valign="top">
-				<td><input type="radio"	name="mss[new]" 				value="0"></td>
-				<td><input type="text"	name="mss[category][1]"		value=""	size="15" /></td>
-				<td><input type="text"	name="mss[type][1]"			value=""	size="5"/></td>
-				<td><input type="text"	name="mss[url][1]"			value=""	size="50" /></td>
+				<td>
+					<select name="mss[category]">
+<?php					echo hierarhical_select($cats);		?>
+					</select>
+				</td>
+				<td>
+					<select name="mss[type]" >
+						<option value="internal">Internal</option>
+						<option value="external">External</option>
+						<option value="blog">Blog</option>
+					</select>
+				</td>
+				<td><input type="text"	name="mss[url]"	value=""	size="75" /></td>
 			</tr>
 		</table>
-		<input type="hidden" name="action" value="update" />
-		<input type="hidden" name="page_options" value="mss_opt" />
+		<input type="hidden" name="new_rss" value="1" />
 		<p class="submit">
 			<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
 		</p>
 	</form>
-		
+	<form method="post" action="">
+		<table class="form-table">
+			<tr><th>Categoría</th><th>Tipo</th><th>URL de RSS</th><th>Quitar</th></tr>
+<?php		foreach($list as $item){		?>
+			<tr><td><?php echo $item->cat;?></td><td><?php echo $item->type;?></td><td><?php echo $item->url;?></td><td><input type="submit" name="delete[<?php echo $item->id;?>]" value="Eliminar" /></td></tr>
+<?php		}		?>
+		</table>
 	</form>
 </div>
+
+<?php 
+//$a = mss_get_rss('conflicto','internal');
+//dump($a);
+?>
