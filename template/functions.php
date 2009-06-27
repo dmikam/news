c09a44ae7f84a75189894e98712fe19932403b43
Daniel Aguilar Castro <dac@dac-macbook.local>
2009-06-27 13:17:21 +0200

    Obtención de destacado principal Creación de la funcion get_featured_category_id(category_parent) para obtener el post destacado Maquetación del destacado principal y estructura de contenido/sidebar

--- template/functions.php
+++ template/functions.php
@@ -1,52 +1,9 @@
 <?php
-/*
-Plugin Name: Nueva Galeria
-Plugin URI: http://nextline.es
-Description: Crea un nuevo "shortcode" para galeria
-Author: nextline
-Version: 1.0
-Author URI: www.nextline.es
-*/
-
-remove_shortcode('gallery');
-add_shortcode('gallery', 'new_gallery_function');
 
 add_shortcode('linea', 'add_clearfix');
 
 function add_clearfix() {
 	return "<div style=\"clear:both\"></div>";
-}
-
-function new_gallery_function($atts){
-	global $post;
-	extract(shortcode_atts(array(
-		'id'			=> $post->ID,
-		'params'		=> 'rel="lightbox[\'gallery\']"',
-		'galid'        	=> '',
-		'galerytag'		=> 'ul',
-		'itemtag'		=> 'li',
-		'captiontag'	=> 'span',
-		'perpage'		=> '3',
-		'size'			=> 'thumbnail',
-		'orderby'		=> 'menu_order ASC, ID ASC'
-	), $atts));
-
-	$id = (int)$id;
-	$images = get_children("post_parent=$id&post_type=attachment&post_mime_type=image&orderby={$orderby}");
-	if (!empty($images)) :
-		$return = "<$galerytag ".(!empty($galid)?" id='$galid' ":"")." class=\"gallery\">";
-		$perpage = (int)$perpage;
-		$i=0;
-		$start = $_GET["$galid_start"];
-		foreach($images as $image) {
-			$meta = wp_get_attachment_metadata($image->ID);
-			//$thumb_path = dirname($image->guid)."/".$meta['sizes'][$size]['file'];
-			$return .= "<$itemtag><a href=\"$image->guid\" $params>" . wp_get_attachment_image($image->ID,$size) . "</a></$itemtag>";
-		}
-		$return .= "</$galerytag>";
-	endif;
-	return $return;
-	
 }
 
 function get_single_image($size = 'thumbnail') {
@@ -60,6 +17,16 @@ function get_single_image($size = 'thumbnail') {
 			return wp_get_attachment_image($image->ID,$size);
 		endforeach;
 	endif;
+}
+
+function get_category_featured_id($category_parent='') {
+	if (empty($category_parent)) :
+		$category = get_category_by_slug('destacado');
+	else :
+		$parent = get_category($category_parent);
+		$category = get_category_by_slug($parent->slug . '-destacado');
+	endif;
+ 	return $category->cat_ID;
 }
 
 ?>
--- template/index.php
+++ template/index.php
@@ -1,6 +1,18 @@
 <?php get_header() ?>
 <div id="container">
 	<div id="content" class="clearfix">
+		<div id="featured" class="clearfix">
+			<? $featured = get_posts("category=" . get_category_featured_id() . "&numberposts=1"); ?>
+		 	<? foreach($featured as $post) : ?>
+		    	<? setup_postdata($post); ?>
+		 		<h2><a href="<?php the_permalink(); ?>" id="post-<?php the_ID(); ?>"><?php the_title(); ?></a></h2>
+				<? echo get_single_image("large"); ?>
+				<div id="excerpt">
+					<? the_excerpt(); ?>
+				</div>
+		 	<?php endforeach; ?>
+		</div>
+		
 		<?php while (have_posts()) : the_post(); ?>
 			<h2 class="title" id="title-<? the_ID(); ?>">
 				<a class="none" href="<?php the_permalink() ?>" rel="bookmark" title="Enlace a <?php the_title_attribute(); ?>">
