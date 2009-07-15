f3c9acb9aafa9326914fe03cfc9539fd0b654bce
Daniel Aguilar Castro <dac@dac-macbook.local>
2009-07-15 20:35:13 +0200

    Etiquetas de noticias y bloques de enlaces

--- template/single.php
+++ template/single.php
@@ -32,34 +32,42 @@
 			<div class="entry">
 					<?php the_content('Leer más &raquo;'); ?>
 			</div>
-			<div class="related_posts">
-				<h3>Artículos relacionados</h3>
-			<?php echo related_posts_shortcode('limit=5');?>
+			
+			<div id="tags">
+				<? the_tags('Etiquetas: ',', '); ?>
 			</div>
-		<?php endwhile; ?>
-<?
-		$more_fields = mf_get_boxes();
-		reset($more_fields["Enlaces recomendados"]["field"]);
-		$title = each($more_fields["Enlaces recomendados"]["field"]);
-		$recomended = '';
-		while($title) { 
-			$title_val = get_meta($title['value']['key']);
-			$link = each($more_fields["Enlaces recomendados"]["field"]);
-			$link_val = get_meta($link['value']['key']);
-			if (!empty($title_val) && !empty($link_val)){
-				$recomended .= "<li><a href=\"$link_val\">$title_val</a></li>";
-			}
-			$title = each($more_fields["Enlaces recomendados"]["field"]);
-		}
-		if (!empty($recomended)){
-?>		
-		<div class="recomended-links">
-			<h3>Enlaces recomendados</h3>
-			<ul>
-				<?echo $recomended;?>
-			</ul>
-		</div>
-<?		}		?>
+			
+			<div id="meta_links" class="clearfi">
+					<div class="related_posts">
+						<h3>Artículos relacionados</h3>
+					<?php echo related_posts_shortcode('limit=5');?>
+					</div>
+				<?php endwhile; ?>
+		<?
+				$more_fields = mf_get_boxes();
+				reset($more_fields["Enlaces recomendados"]["field"]);
+				$title = each($more_fields["Enlaces recomendados"]["field"]);
+				$recomended = '';
+				while($title) { 
+					$title_val = get_meta($title['value']['key']);
+					$link = each($more_fields["Enlaces recomendados"]["field"]);
+					$link_val = get_meta($link['value']['key']);
+					if (!empty($title_val) && !empty($link_val)){
+						$recomended .= "<li><a href=\"$link_val\">$title_val</a></li>";
+					}
+					$title = each($more_fields["Enlaces recomendados"]["field"]);
+				}
+				if (!empty($recomended)){
+		?>		
+				<div class="recomended-links">
+					<h3>Enlaces recomendados</h3>
+					<ul>
+						<?echo $recomended;?>
+					</ul>
+				</div>
+		<?		}		?>
+			</div>
+		
 	</div>
 <?php get_sidebar() ?>
 </div>
