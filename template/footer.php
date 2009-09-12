0c374837dd3579229398f4e12918286da112ecfc
Daniel Aguilar Castro <dac@dac-macbook.local>
2009-09-12 11:35:46 +0200

    Lista de categorías en cabecera (con submenu) y footer, planas

--- template/footer.php
+++ template/footer.php
@@ -4,6 +4,8 @@
 			<h3>Más en Periodismo Humano</h3>
 			<ul>
 				<? wp_list_pages('title_li='); ?>
+				<? wp_list_categories('hierarchical=0&title_li=&hide_empty=0&orderby=term_order&exclude=39,24,25,26,310,311,313,314,309,40,308'); ?>
+				
 			</ul>
 		</div>
 	</div>
