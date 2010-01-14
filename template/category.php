8924089da8a1478c728372a64742ba28f3192cce
Daniel Aguilar Castro <dac@dac-macbook.local>
2010-01-14 18:26:27 +0100

    Portadilla de sección

--- template/category.php
+++ template/category.php
@@ -1,6 +1,6 @@
 <?php get_header() ?>
 <div id="container" class="clearfix">
-	<div id="content" class="clearfix">
+	<div id="content" class="category-page clearfix">
 		<? $featured_cat = get_category_featured_id(intval(get_query_var('cat'))); ?>
 		<? if (!empty($featured_cat)) : ?>
 			<? $featured = get_posts("category=" . $featured_cat . "&numberposts=1"); ?>
@@ -27,17 +27,32 @@
 		<div id="sub-content" class="clearfix">
 				<?php while (have_posts()) : the_post(); ?>
 					<? if ($featured_post_id != get_the_ID()) : ?>
-						<div class="category_news">
-							<h2 class="title" id="title-<? the_ID(); ?>">
-								<a href="<?php the_permalink() ?>" title="Enlace a <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-							</h2>
-							<div class="entry">
+						<div class="clearfix category_news">
+							<div class="category_news_image">
 								<? $wimage = get_meta('image'); ?>
 								<? if (!empty($wimage)) :  ?>
-									<a href="<?php the_permalink(); ?>" title="<? the_title(); ?>">	<? echo get_single_image(array(630,630)); ?></a>
+									<a href="<?php the_permalink(); ?>" title="<? the_title(); ?>">	<? echo get_single_image(array(291,630)); ?></a>
+									<? $class = "with_image";?>
+								<? else: ?>
+									<? $class = "no_image";?>
 								<? endif; ?>
-								<?php echo apply_filters( 'the_content', get_the_excerpt()); ?>
 							</div>
+							<div class="category_news_post <? echo $class; ?> ">
+								<h2 class="title" id="title-<? the_ID(); ?>">
+									<a href="<?php the_permalink() ?>" title="Enlace a <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+								</h2>
+								<h5 class="metadata">
+								<span class="date"><?	echo date('d.m.Y',strtotime($post->post_date));  ?></span> · <span class="author"><? the_author_posts_link(); ?></span> 
+								</h5>
+								<div class="entry">
+									<?php echo apply_filters( 'the_content', get_the_excerpt()); ?>
+									
+									<h5 class="read_more">
+										<a href="<?php the_permalink() ?>" title="Leer el resto del artículo <?php the_title_attribute(); ?>">Leer más</a>
+									</h5>
+								</div>
+							</div>
+							
 						</div>
 					<? endif; // Si el posts es distinto al posts destacado, para no duplicar el post en la portadilla ?>
 				<?php endwhile; ?>
