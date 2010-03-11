608fe893b4aac8aa98e6a2a99eff578cc46d6540
Daniel Aguilar Castro <dac@dac-macbook.local>
2010-03-11 15:46:53 +0100

    Paginas de blogs simples Beta en titulo Retoques estilos

--- blog-basic/page.php
+++ blog-basic/page.php
@@ -1,16 +1,29 @@
 <?php get_header() ?>
-<div id="container">
-	<div id="content" class="clearfix">
 		<?php while (have_posts()) : the_post(); ?>
-			<h2 class="title" id="title-<? the_ID(); ?>">
-				<a class="none" href="<?php the_permalink() ?>" rel="bookmark" title="Enlace a <?php the_title_attribute(); ?>">
-					<?php the_title(); ?>
-				</a>
-			</h2>
-			<div class="entry">
-					<?php the_content('Leer más &raquo;'); ?>
+			<div class="top clearfix">
+				<h2 class="title" id="title-<? the_ID(); ?>">
+					<a class="none" href="<?php the_permalink() ?>" rel="bookmark" title="Enlace a <?php the_title_attribute(); ?>">
+						<?php the_title(); ?>
+					</a>
+				</h2>
+				<div class="comments">
+					<a href="<?php the_permalink() ?>#comments">
+						<?php comments_number('(0) comentarios ','(1) comentario','(%) comentarios');?>
+					</a>
+				</div>
 			</div>
+			<h5 class="metadata">
+				<span class="date"><?	echo date('d.m.Y',strtotime($post->post_date));  ?></span> · <span class="author"><? the_author_posts_link(); ?></span> 
+			</h5>
+
+			<div class="entry">
+					<?php the_content('Leer más'); ?>
+			</div>
+			
+		
 		<?php endwhile; ?>
+		<?php comments_template(); ?>
+		
 	</div>
 <?php get_sidebar() ?>
 </div>
--- template/functions.php
+++ template/functions.php
@@ -352,4 +352,12 @@ function comentarios($comment, $args, $depth) {
 add_action('wp','proximamente');
 
 add_filter('init', create_function('$a', 'global $wp_rewrite; $wp_rewrite->author_base = "autor"; $wp_rewrite->flush_rules();'));
+
+if ( current_user_can('contributor') && !current_user_can('upload_files') )
+	add_action('admin_init', 'allow_contributor_uploads');
+ 
+function allow_contributor_uploads() {
+	$contributor = get_role('contributor');
+	$contributor->add_cap('upload_files');
+}
 ?>
